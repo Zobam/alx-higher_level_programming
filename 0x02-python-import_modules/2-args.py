b2d6b179b5845f5072956954b8e75e4b2eac3f4e
Chizoba <upc4you@gmail.com>
2022-07-04 12:10:05 -0700

    added double line spaces

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def principal():
     print('{} argument'.format(len(argv) - 1), end='')
     if len(argv) == 1:
@@ -11,6 +12,7 @@ def principal():
         print('s:')
     for i in range(1, len(argv)):
         print('{}: {}'.format(i, argv[i]))
+
 
 if __name__ == "__main__":
     principal()
