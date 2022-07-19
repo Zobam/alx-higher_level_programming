e96b59bcd269d393c857c797c5d38e12e296df8c
Chizoba <upc4you@gmail.com>
2022-07-18 20:28:33 -0700

    fixed identing error in task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-# 0-safe_print_list.py
-
 def safe_print_list(my_list=[], x=0):
-    """prints x elements of a list"""
-    y = 0
-    for i in range(x):
-        try:
-            print(my_list[index], end='')
-            y += 1
-        except Exception as error:
-            break
-        print('')
-        return y
+    try:
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        print()
+        return i
+    print()
+    return x
